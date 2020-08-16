e4010fc0d2cc7011f0c10f78553ee07526e91f9e
Ghazi Naceur <ghazi.ennacer@gmail.com>
2020-08-16 19:12:20 +0200

    WIP : Add Spark Kafka plugin producer

---
+++ src/main/scala/io/oss/data/highway/model/DataType.scala
@@ -0,0 +1,7 @@
+package io.oss.data.highway.model
+
+sealed trait DataType
+
+case object JSON extends DataType
+case object CSV extends DataType
+case object PARQUET extends DataType
--- src/main/scala/io/oss/data/highway/model/KafkaMode.scala
+++ src/main/scala/io/oss/data/highway/model/KafkaMode.scala
@@ -6,9 +6,14 @@ case class ProducerConsumer(useConsumer: Boolean,
                             offset: Offset,
                             consumerGroup: String)
     extends KafkaMode
+// TODO Use an intermediate topic and remove out-streams from config + keep "out" as a the output topic
 case class KafkaStreaming(outStreams: String,
                           useConsumer: Boolean,
                           offset: Offset,
                           consumerGroup: String)
     extends KafkaMode
-case object SparkKafkaPlugin extends KafkaMode
+case class SparkKafkaPlugin(useConsumer: Boolean,
+                            offset: Offset,
+                            consumerGroup: String,
+                            useStream: Boolean)
+    extends KafkaMode
