ab6b3c7666e89425e6364830dd8136158677e028
Ghazi Naceur <ghazi.ennacer@gmail.com>
2020-08-19 20:44:27 +0200

    Refactor methods

--- src/main/scala/io/oss/data/highway/converter/KafkaSink.scala
+++ src/main/scala/io/oss/data/highway/converter/KafkaSink.scala
@@ -25,6 +25,8 @@ import cats.syntax.either._
 import io.oss.data.highway.utils.DataFrameUtils.sparkSession
 import org.apache.spark.sql.functions.lit
 
+import scala.sys.ShutdownHookThread
+
 class KafkaSink {
 
   val logger: Logger = Logger.getLogger(classOf[KafkaSink].getName)
@@ -47,82 +49,112 @@ class KafkaSink {
                           offset,
                           consumerGroup) =>
         send(jsonPath, topic, producer)
-
-        Either.catchNonFatal {
-          import org.apache.kafka.streams.scala.ImplicitConversions._
-          import org.apache.kafka.streams.scala.Serdes._
-
-          val props = new Properties
-          props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-          props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-app")
-          props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
-                    Serdes.String().getClass)
-          props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
-                    Serdes.String().getClass)
-          val builder = new StreamsBuilder
-
-          val dataKStream = builder.stream[String, String](topic)
-          dataKStream.to(streamsOutputTopic)
-
-          val streams = new KafkaStreams(builder.build(), props)
-
-          streams.start()
-
-          consume(streamsOutputTopic,
+        runStream(topic,
                   bootstrapServers,
+                  streamsOutputTopic,
                   useConsumer,
                   offset,
                   consumerGroup)
-
-          sys.ShutdownHookThread {
-            streams.close(Duration.ofSeconds(10))
-          }
-        }
       case SparkKafkaPlugin(useConsumer, offset, consumerGroup, useStream) =>
+        // TODO consumer is ont implemented yet for both cases
         if (useStream) {
-          DataFrameUtils
-            .loadDataFrame(jsonPath, JSON)
-            .map(df => {
-
-              // Use an intermediate topic
-              send(jsonPath, "json-to-kafka-streaming-topic", producer)
-
-              val kafkaStream = sparkSession.readStream
-                .format("kafka")
-                .option("kafka.bootstrap.servers", bootstrapServers)
-                .option("startingOffsets", "latest")
-                .option("subscribe", "json-to-kafka-streaming-topic")
-                .load()
-
-              val dff =
-                kafkaStream.withColumn("uuid", lit(UUID.randomUUID().toString))
-              dff
-                .selectExpr("CAST(value AS STRING)")
-                .writeStream
-                .format("kafka") // console
-                .option("truncate", false)
-                .option("kafka.bootstrap.servers", bootstrapServers)
-                .option("checkpointLocation", "/tmp/data-highway/checkpoint")
-                .option("topic", topic)
-                .start()
-                .awaitTermination()
-            })
+          sendUsingStreamSparkKafkaPlugin(jsonPath,
+                                          producer,
+                                          bootstrapServers,
+                                          topic)
         } else {
-          import org.apache.spark.sql.functions._
-          DataFrameUtils
-            .loadDataFrame(jsonPath, JSON)
-            .map(df => {
-              val dff = df.withColumn("uuid", lit(UUID.randomUUID().toString))
-              dff
-                .select(col("uuid").cast("string").as("key"),
-                        to_json(struct("*")).as("value"))
-                .write
-                .format("kafka")
-                .option("kafka.bootstrap.servers", bootstrapServers)
-                .option("topic", topic)
-                .save()
-            })
+          sendUsingSparkKafkaPlugin(jsonPath, bootstrapServers, topic)
         }
+    }
+  }
+
+  private def sendUsingSparkKafkaPlugin(
+      jsonPath: String,
+      bootstrapServers: String,
+      topic: String): Either[Throwable, Unit] = {
+    import org.apache.spark.sql.functions._
+    DataFrameUtils
+      .loadDataFrame(jsonPath, JSON)
+      .map(df => {
+        val dff = df.withColumn("uuid", lit(UUID.randomUUID().toString))
+        dff
+          .select(col("uuid").cast("string").as("key"),
+                  to_json(struct("*")).as("value"))
+          .write
+          .format("kafka")
+          .option("kafka.bootstrap.servers", bootstrapServers)
+          .option("topic", topic)
+          .save()
+      })
+  }
+
+  private def sendUsingStreamSparkKafkaPlugin(
+      jsonPath: String,
+      producer: KafkaProducer[String, String],
+      bootstrapServers: String,
+      topic: String): Either[Throwable, Unit] = {
+    Either.catchNonFatal {
+      // Use an intermediate topic
+      send(jsonPath, "json-to-kafka-streaming-topic", producer)
+
+      val kafkaStream = sparkSession.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", bootstrapServers)
+        .option("startingOffsets", "latest")
+        .option("subscribe", "json-to-kafka-streaming-topic")
+        .load()
+
+      val dff =
+        kafkaStream.withColumn("uuid", lit(UUID.randomUUID().toString))
+      dff
+        .selectExpr("CAST(value AS STRING)")
+        .writeStream
+        .format("kafka") // console
+        .option("truncate", false)
+        .option("kafka.bootstrap.servers", bootstrapServers)
+        .option("checkpointLocation", "/tmp/data-highway/checkpoint")
+        .option("topic", topic)
+        .start()
+        .awaitTermination()
+    }
+  }
+
+  private def runStream(
+      topic: String,
+      bootstrapServers: String,
+      streamsOutputTopic: String,
+      useConsumer: Boolean,
+      offset: Offset,
+      consumerGroup: String): Either[Throwable, ShutdownHookThread] = {
+    Either.catchNonFatal {
+      import org.apache.kafka.streams.scala.ImplicitConversions._
+      import org.apache.kafka.streams.scala.Serdes._
+
+      val props = new Properties
+      props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+      props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-app")
+      props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
+                Serdes.String().getClass)
+      props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
+                Serdes.String().getClass)
+      val builder = new StreamsBuilder
+
+      val dataKStream = builder.stream[String, String](topic)
+      dataKStream.to(streamsOutputTopic)
+
+      val streams = new KafkaStreams(builder.build(), props)
+
+      streams.start()
+
+      consume(streamsOutputTopic,
+              bootstrapServers,
+              useConsumer,
+              offset,
+              consumerGroup)
+
+      sys.ShutdownHookThread {
+        streams.close(Duration.ofSeconds(10))
+      }
     }
   }
 
