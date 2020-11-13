9fee9cea3707722dd09b09250c389fc82e1876a9
ghazi-naceur <ghazi.ennacer@gmail.com>
2020-11-13 23:58:31 +0100

    (wip) Rearrange Dockerfiles and create a specific image for data-highway-kafka

---
+++ docker/kafka/cleanup_and_rebuild.sh
@@ -0,0 +1,14 @@
+# Delete all containers
+docker rm -f $(docker ps -aq)
+# Delete all images
+docker rmi -f $(docker images -a -q)
+docker build -t data-highway-kafka:v1.0 .
+#docker run -tid --name bungee-gum data-highway:v1.0
+docker run -tid \
+  -v /home/ghazi/playgroud/data-highway/shell/csv_to_parquet-data/input/:/app/data/input \
+  -v /home/ghazi/playgroud/data-highway/shell/csv_to_parquet-data/output/:/app/data/output \
+  --name bungee-gum-kafka data-highway-kafka:v1.0
+docker ps -a
+docker images
+#docker exec -ti bungee-gum-kafka java -jar -Dconfig.file=/home/ghazi/playgroud/data-highway/shell/kafka-conf/application.conf \
+#    target/scala-2.12/data-highway-assembly-0.1.jar
--- docker/spark/cleanup_and_rebuild.sh
+++ docker/spark/cleanup_and_rebuild.sh
@@ -2,15 +2,15 @@
 docker rm -f $(docker ps -aq)
 # Delete all images
 docker rmi -f $(docker images -a -q)
-docker build -t data-highway:v1.0 .
+docker build -t data-highway-spark:v1.0 .
 #docker run -tid --name bungee-gum data-highway:v1.0
 docker run -tid \
   -v /home/ghazi/playgroud/data-highway/shell/csv_to_parquet-data/input/:/app/data/input \
   -v /home/ghazi/playgroud/data-highway/shell/csv_to_parquet-data/output/:/app/data/output \
-  --name bungee-gum data-highway:v1.0
+  --name bungee-gum-spark data-highway-spark:v1.0
 docker ps -a
 docker images
-docker exec -ti bungee-gum spark-submit  \
+docker exec -ti bungee-gum-spark spark-submit  \
       --packages org.apache.spark:spark-avro_2.12:2.4.0 \
       --class "io.oss.data.highway.App" --master local[*] \
       --conf "spark.driver.extraJavaOptions=-Dconfig.file=/app/config/application.conf -Dlog4j2.configuration=/app/config/log4j2.properties" \
