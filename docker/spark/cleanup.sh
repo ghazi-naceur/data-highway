99d999157da8f1ed3dc880d5af1487c75978df3d
ghazi-naceur <ghazi.ennacer@gmail.com>
2020-12-14 00:15:37 +0100

    (WIP) data-highway rest api docker image

--- docker/spark/cleanup.sh
+++ docker/spark/cleanup.sh
@@ -2,3 +2,5 @@
 docker rm -f $(docker ps -aq)
 # Delete all images
 docker rmi -f $(docker images -a -q)
+
+docker network rm $(docker network ls -q)
--- docker/spark/start.sh
+++ docker/spark/start.sh
@@ -1,1 +1,3 @@
-docker-compose up -d
+docker-compose run --service-ports app java -jar -Dconfig.file=/app/config/application.conf -Dlog4j2.configuration=/app/config/log4j2.properties /app/jar/data-highway-assembly-0.1.jar
+docker-compose run --service-ports app java -cp /app/jar/data-highway-assembly-0.1.jar io.oss.data.highway.IOMain -Dconfig.file=/app/config/application.conf -Dlog4j2.configuration=/app/config/log4j2.properties
+docker-compose run  -p 5555:5555 app java -cp /app/jar/data-highway-assembly-0.1.jar io.oss.data.highway.IOMain -Dconfig.file=/app/config/application.conf -Dlog4j2.configuration=/app/config/log4j2.properties
