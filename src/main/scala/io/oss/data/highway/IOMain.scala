e7f107f8270423810f270dc80feba3b06fa09739
ghazi-naceur <ghazi.ennacer@gmail.com>
2021-02-06 08:06:09 +0100

    Add custom banner

--- src/main/scala/io/oss/data/highway/IOMain.scala
+++ src/main/scala/io/oss/data/highway/IOMain.scala
@@ -5,6 +5,7 @@ import org.http4s.server.blaze._
 import io.oss.data.highway.rest.ConversionController
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.Source
 
 object IOMain extends IOApp {
 
@@ -15,10 +16,15 @@ object IOMain extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
 
     BlazeServerBuilder[IO](global)
+      .withBanner(getBanner)
       .bindHttp(5555, "localhost")
       .withHttpApp(ConversionController.httpRequests)
       .resource
       .use(_ => IO.never)
       .as(ExitCode.Success)
+  }
+
+  def getBanner: List[String] = {
+    Source.fromResource("banner.txt").getLines().toList
   }
 }
