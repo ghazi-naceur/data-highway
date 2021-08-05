feb468f3f5865471cf9105c393c42e011377228e
ghazi-naceur <ghazi.ennacer@gmail.com>
2021-08-05 21:17:40 +0200

    Rename file system property to storage

--- src/main/scala/io/oss/data/highway/Main.scala
+++ src/main/scala/io/oss/data/highway/Main.scala
@@ -38,43 +38,43 @@ object Main {
   def apply(route: Route): Either[Throwable, Any] = {
     logger.info(s"${route.toString} route is activated ...")
     route match {
-      case CsvToParquet(in, out, fileSystem) =>
-        ParquetSink.handleParquetChannel(in, out, Overwrite, fileSystem, CSV)
-      case JsonToParquet(in, out, fileSystem) =>
-        ParquetSink.handleParquetChannel(in, out, Overwrite, fileSystem, JSON)
-      case AvroToParquet(in, out, fileSystem) =>
-        ParquetSink.handleParquetChannel(in, out, Overwrite, fileSystem, AVRO)
-      case XlsxToCsv(in, out, fileSystem) =>
-        CsvSink.handleCsvChannel(in, out, Overwrite, fileSystem, XLSX)
-      case ParquetToCsv(in, out, fileSystem) =>
-        CsvSink.handleCsvChannel(in, out, Overwrite, fileSystem, PARQUET)
-      case AvroToCsv(in, out, fileSystem) =>
-        CsvSink.handleCsvChannel(in, out, Overwrite, fileSystem, AVRO)
-      case JsonToCsv(in, out, fileSystem) =>
-        CsvSink.handleCsvChannel(in, out, Overwrite, fileSystem, JSON)
-      case ParquetToJson(in, out, fileSystem) =>
-        JsonSink.handleJsonChannel(in, out, Overwrite, fileSystem, PARQUET)
-      case AvroToJson(in, out, fileSystem) =>
-        JsonSink.handleJsonChannel(in, out, Overwrite, fileSystem, AVRO)
-      case CsvToJson(in, out, fileSystem) =>
-        JsonSink.handleJsonChannel(in, out, Overwrite, fileSystem, CSV)
-      case ParquetToAvro(in, out, fileSystem) =>
-        AvroSink.handleAvroChannel(in, out, Overwrite, fileSystem, PARQUET)
-      case JsonToAvro(in, out, fileSystem) =>
-        AvroSink.handleAvroChannel(in, out, Overwrite, fileSystem, JSON)
-      case CsvToAvro(in, out, fileSystem) =>
-        AvroSink.handleAvroChannel(in, out, Overwrite, fileSystem, CSV)
-      case KafkaToFile(in, out, fileSystem, kafkaMode) =>
-        KafkaSampler.consumeFromTopic(in, out, fileSystem, kafkaMode)
-      case FileToKafka(in, out, fileSystem, kafkaMode) =>
-        KafkaSink.publishToTopic(in, out, fileSystem, kafkaMode)
+      case CsvToParquet(in, out, storage) =>
+        ParquetSink.handleParquetChannel(in, out, Overwrite, storage, CSV)
+      case JsonToParquet(in, out, storage) =>
+        ParquetSink.handleParquetChannel(in, out, Overwrite, storage, JSON)
+      case AvroToParquet(in, out, storage) =>
+        ParquetSink.handleParquetChannel(in, out, Overwrite, storage, AVRO)
+      case XlsxToCsv(in, out, storage) =>
+        CsvSink.handleCsvChannel(in, out, Overwrite, storage, XLSX)
+      case ParquetToCsv(in, out, storage) =>
+        CsvSink.handleCsvChannel(in, out, Overwrite, storage, PARQUET)
+      case AvroToCsv(in, out, storage) =>
+        CsvSink.handleCsvChannel(in, out, Overwrite, storage, AVRO)
+      case JsonToCsv(in, out, storage) =>
+        CsvSink.handleCsvChannel(in, out, Overwrite, storage, JSON)
+      case ParquetToJson(in, out, storage) =>
+        JsonSink.handleJsonChannel(in, out, Overwrite, storage, PARQUET)
+      case AvroToJson(in, out, storage) =>
+        JsonSink.handleJsonChannel(in, out, Overwrite, storage, AVRO)
+      case CsvToJson(in, out, storage) =>
+        JsonSink.handleJsonChannel(in, out, Overwrite, storage, CSV)
+      case ParquetToAvro(in, out, storage) =>
+        AvroSink.handleAvroChannel(in, out, Overwrite, storage, PARQUET)
+      case JsonToAvro(in, out, storage) =>
+        AvroSink.handleAvroChannel(in, out, Overwrite, storage, JSON)
+      case CsvToAvro(in, out, storage) =>
+        AvroSink.handleAvroChannel(in, out, Overwrite, storage, CSV)
+      case KafkaToFile(in, out, storage, kafkaMode) =>
+        KafkaSampler.consumeFromTopic(in, out, storage, kafkaMode)
+      case FileToKafka(in, out, storage, kafkaMode) =>
+        KafkaSink.publishToTopic(in, out, storage, kafkaMode)
       case KafkaToKafka(in, out, kafkaMode) =>
         // todo split publishToTopic between FTK and KTK, and omit Local
         KafkaSink.publishToTopic(in, out, Local, kafkaMode)
-      case FileToElasticsearch(in, out, fileSystem, bulkEnabled) =>
-        ElasticSink.handleElasticsearchChannel(in, out, fileSystem, bulkEnabled)
-      case ElasticsearchToFile(in, out, fileSystem, searchQuery) =>
-        ElasticSampler.saveDocuments(in, out, fileSystem, searchQuery)
+      case FileToElasticsearch(in, out, storage, bulkEnabled) =>
+        ElasticSink.handleElasticsearchChannel(in, out, storage, bulkEnabled)
+      case ElasticsearchToFile(in, out, storage, searchQuery) =>
+        ElasticSampler.saveDocuments(in, out, storage, searchQuery)
       case ElasticOps(operation) =>
         ElasticAdminOps.execute(operation)
       case FileToCassandra(in, cassandra, storage, dataType) =>
