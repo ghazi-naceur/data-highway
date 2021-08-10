dc94721ed00a0568f8374446e2d0ef6ae91644a5
ghazi-naceur <ghazi.ennacer@gmail.com>
2021-08-11 01:10:15 +0200

    Redefining route's structure

--- src/main/scala/io/oss/data/highway/models/Route.scala
+++ src/main/scala/io/oss/data/highway/models/Route.scala
@@ -1,5 +1,7 @@
 package io.oss.data.highway.models
 
+import pureconfig.generic.semiauto.deriveEnumerationReader
+
 sealed trait Route
 
 case class XlsxToCsv(in: String, out: String, storage: Storage) extends Route
@@ -56,3 +58,46 @@ case class FileToCassandra(in: String, cassandra: Cassandra, storage: Storage, d
     extends Route
 
 case class CassandraToFile(cassandra: Cassandra, out: String, dataType: DataType) extends Route
+
+sealed trait Input
+sealed trait Output
+
+case class RouteBis(input: Input, output: Output)
+case class File(dataType: DataType, path: String)        extends Input with Output
+case class CassandraBis(keyspace: String, table: String) extends Input with Output
+case class Elasticsearch(index: String)                  extends Input with Output
+case class Kafka(topic: String, kafkaMode: KafkaMode)    extends Input with Output
+
+// todo to be removed
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(loadRouteBisConf())
+//    RouteBis(File(CSV, ""), CassandraBis("", "")) match {
+//      case RouteBis(input: File, output: File)          =>
+//      case RouteBis(input: File, output: CassandraBis)  =>
+//      case RouteBis(input: CassandraBis, output: File)  =>
+//      case RouteBis(input: File, output: Kafka)         => // todo only json is supported, make other types supported too
+//      case RouteBis(input: Kafka, output: File)         => // todo only json is supported, make other types supported too
+//      case RouteBis(input: File, output: Elasticsearch) => // todo only json is supported, make other types supported too
+//      case RouteBis(input: Elasticsearch, output: File) => // todo only json is supported, make other types supported too
+//    }
+
+    def loadRouteBisConf(): RouteBis = {
+      import pureconfig._
+      import pureconfig.generic.auto._ // To be kept, even though intellij didn't recognize its usage
+
+      implicit val offsetConvert: ConfigReader[LogLevel] =
+        deriveEnumerationReader[LogLevel]
+
+      ConfigSource.default
+        .at("route-bis")
+        .load[RouteBis] match {
+        case Right(conf) => conf
+        case Left(thr) =>
+          throw new RuntimeException(
+            s"Error when trying to load Spark configuration : ${thr.toList.mkString("\n")}"
+          )
+      }
+    }
+  }
+}
