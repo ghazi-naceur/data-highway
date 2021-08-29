1fb0da032bcf4f072c72e311e0f84d175ed48f31
ghazi-naceur <ghazi.ennacer@gmail.com>
2021-08-29 11:57:16 +0200

    Generalize handling POST REST queries

--- src/main/scala/io/oss/data/highway/controllers/ConversionController.scala
+++ src/main/scala/io/oss/data/highway/controllers/ConversionController.scala
@@ -2,49 +2,47 @@ package io.oss.data.highway.controllers
 
 import cats.data.Kleisli
 import cats.effect._
-import org.http4s.implicits._
 import io.circe.syntax._
 import io.oss.data.highway.configs.ConfigLoader
 import io.oss.data.highway.engine.Dispatcher
-import io.oss.data.highway.models.{Query, Route}
+import io.oss.data.highway.models.{Route, Query}
 import org.apache.log4j.Logger
 import org.http4s._
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.circe._
 import org.http4s.dsl.io._
+import org.http4s.implicits._
 
 object ConversionController {
 
   val logger: Logger = Logger.getLogger(ConversionController.getClass.getName)
-
-  import pureconfig.generic.auto._ // To be kept, even though intellij didn't recognize its usage
   val httpRequests: Kleisli[IO, Request[IO], Response[IO]] = HttpRoutes
     .of[IO] {
       case req @ GET -> Root / "conversion" =>
         logger.info("GET Request received : " + req.toString())
         Ok(s"Data Highway REST API.")
       case req @ POST -> Root / "conversion" / "query" =>
-        logger.info("POST Request received : " + req.toString())
-        val ioResponse = req.asJson.map(request => {
-          val decodedOps =
-            ConfigLoader().loadConfigsFromString[Query]("query", request.asJson.toString())
-          Dispatcher.apply(decodedOps)
-        })
-        ioResponse.flatMap {
-          case Right(_)        => Ok(200)
-          case Left(exception) => throw new RuntimeException(exception)
-        }
+        handleRestQuery("query", req)
       case req @ POST -> Root / "conversion" / "route" =>
-        logger.info("POST Request received : " + req.toString())
-        val ioResponse = req.asJson.map(request => {
-          val decodedRoute =
-            ConfigLoader().loadConfigsFromString[Route]("route", request.asJson.toString())
-          Dispatcher.apply(decodedRoute)
-        })
-        ioResponse.flatMap {
-          case Right(_)        => Ok(200)
-          case Left(exception) => throw new RuntimeException(exception)
-        }
+        handleRestQuery("route", req)
     }
     .orNotFound
+
+  private def handleRestQuery(param: String, req: Request[IO]): IO[Response[IO]] = {
+    import pureconfig.generic.auto._
+    logger.info("POST Request received : " + req.toString())
+    val ioResponse = req.asJson.map(request => {
+      val parsedRestQuery = param match {
+        case "route" =>
+          ConfigLoader().loadConfigsFromString[Route](param, request.asJson.toString())
+        case "query" =>
+          ConfigLoader().loadConfigsFromString[Query](param, request.asJson.toString())
+      }
+      Dispatcher.apply(parsedRestQuery)
+    })
+    ioResponse.flatMap {
+      case Right(_)        => Ok(200)
+      case Left(exception) => throw new RuntimeException(exception)
+    }
+  }
 }
